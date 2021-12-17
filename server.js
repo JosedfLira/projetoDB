502f02a5487139651f455e1e0adc7498e1730c35
JosedfLira <95888106+JosedfLira@users.noreply.github.com>
2021-12-16 21:20:51 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log("testando nosso package.json com o comando node")
+//console.log("testando nosso package.json com o comando node")
 
 const express = require('express') 
 const app = express()
@@ -12,3 +12,38 @@ app.get('/', (req,res) => {
 })
 
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended:true}))
+
+app.post('/show', (req,res) => {
+    console.log('req.body')
+})
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://dbUser:dbUser@cluster0.jwmmh.mongodb.net/projetoDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => { const collection = client.db("test").collection("devices");
+
+MongoClient.connect(uri, (err, client) => {
+    if(err) return console.log(err)
+
+    db = client.db('teste-bd')
+
+    app.listen(3000, () =>{
+        console.log("servidor rodando tranquilo")
+    })
+})
+
+app.get('/', (req, res) => {
+    let cursor = db.collection('crud').find()
+}) 
+
+app.get('/show', (req, res) => {
+    db.collection('crud').find()toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', {crud: results})
+    })
+})
+
+
+
